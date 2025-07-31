d98f5a7ddf70c0bff2cbae9590ef68ea7559d96b
Hamza Ahmad Khan <ihamzahmadkhan@gmail.com>
2025-08-01 02:05:34 +0500

    add light and dark modd

--- lib/view/home_screen.dart
+++ lib/view/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_statemanagement/view/theme_change.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,8 +13,25 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(children: []),
+      appBar: AppBar(title: Text("Home Screen")),
+      body: Center(
+        child: Column(
+          children: [
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  DialogRoute(
+                    context: context,
+                    builder: (context) => ThemeChange(),
+                  ),
+                );
+              },
+              icon: Icon(Icons.navigation_rounded),
+            ),
+          ],
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.snackbar(
---
+++ lib/view/theme_change.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class ThemeChange extends StatefulWidget {
+  const ThemeChange({super.key});
+
+  @override
+  State<ThemeChange> createState() => _ThemeChangeState();
+}
+
+class _ThemeChangeState extends State<ThemeChange> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ListTile(
+              leading: const Icon(Icons.light),
+              title: const Text("Light Theme"),
+              onTap: () {
+                Get.changeTheme(ThemeData.light());
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.dark_mode),
+              title: const Text("Dart Theme"),
+              onTap: () {
+                Get.changeTheme(ThemeData.dark());
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
